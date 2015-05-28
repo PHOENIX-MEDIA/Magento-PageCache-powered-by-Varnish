b942b97a2a6faed58489b29ac5bfbdfc4336e2c7
Cornelius Adams <cornelius.adams@conlabz.de>
2015-05-28 22:12:36 +0200

    added the ability to cache secure pages

--- app/code/community/Phoenix/VarnishCache/Helper/Cache.php
+++ app/code/community/Phoenix/VarnishCache/Helper/Cache.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * PageCache powered by Varnish
- * 
+ *
  * NOTICE OF LICENSE
- * 
+ *
  * This source file is subject to the Open Software License (OSL 3.0)
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
@@ -11,7 +11,7 @@
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
  * to support@phoenix-media.eu so we can send you a copy immediately.
- * 
+ *
  * @category   Phoenix
  * @package    Phoenix_VarnishCache
  * @copyright  Copyright (c) 2011-2014 PHOENIX MEDIA GmbH (http://www.phoenix-media.eu)
@@ -25,6 +25,7 @@ class Phoenix_VarnishCache_Helper_Cache extends Mage_Core_Helper_Abstract
     const XML_PATH_VARNISH_CACHE_DISABLE_ROUTES       = 'varnishcache/general/disable_routes';
     const XML_PATH_VARNISH_CACHE_TTL                  = 'varnishcache/general/ttl';
     const XML_PATH_VARNISH_CACHE_ROUTES_TTL           = 'varnishcache/general/routes_ttl';
+    const XML_PATH_VARNISH_CACHE_ENABLE_CACHING_HTTPS = 'varnishcache/general/enable_caching_https';
 
     const REGISTRY_VAR_VARNISH_CACHE_CONTROL_HEADERS_SET_FLAG = '_varnish_cache_control_headers_set_flag';
 
@@ -87,7 +88,10 @@ class Phoenix_VarnishCache_Helper_Cache extends Mage_Core_Helper_Abstract
         }
 
         // disable caching of secure pages
-        if (Mage::app()->getStore()->isCurrentlySecure()) {
+        if (
+            Mage::app()->getStore()->isCurrentlySecure() &&
+            !Mage::getStoreConfigFlag(self::XML_PATH_VARNISH_CACHE_ENABLE_CACHING_HTTPS)
+        ) {
             return $this->setNoCacheHeader();
         }
 
@@ -200,7 +204,6 @@ class Phoenix_VarnishCache_Helper_Cache extends Mage_Core_Helper_Abstract
      */
     public function setNoCacheCookie($renewOnly = false)
     {
-
         if ($this->getCookie()->get(self::NO_CACHE_COOKIE)) {
             $this->getCookie()->renew(self::NO_CACHE_COOKIE);
         } elseif (!$renewOnly) {
